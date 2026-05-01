9ac01cc29d46a035bc14fcb343e66798e6f750b8
arm1nt <bmini8664@gmail.com>
2026-05-01 18:51:11 +0200

    Add functions to read and write MSRs

--- hypervisor/src/arch/x64/include/processor.h
+++ hypervisor/src/arch/x64/include/processor.h
@@ -1,6 +1,7 @@
 #ifndef _HYVEMIND_X64_PROCESSOR_H
 #define _HYVEMIND_X64_PROCESSOR_H
 
+#include "types.h"
 #include <stdint.h>
 
 #define ACCESS_CONTROL_REGISTER(reg)                        \
@@ -16,6 +17,31 @@
 
 ACCESS_CONTROL_REGISTER(cr4)
 #undef ACCESS_CONTROL_REGISTER
+
+static inline uint64_t
+read_msr(const uint64_t msr_nr)
+{
+    uint32_t edx;
+    uint32_t eax;
+
+    asm volatile(
+        "rdmsr"
+        : "=a"(eax), "=d"(edx)
+        : "c"(msr_nr)
+    );
+
+    /* edx || eax */
+    const uint64_t recombined = ((U64(0) | edx) << 32) | eax;
+    return recombined;
+}
+
+static inline void
+write_msr(const uint64_t msr_nr, const uint64_t val)
+{
+    const uint32_t edx = val >> 32;
+    const uint32_t eax = (uint32_t) ((val << 32) >> 32);
+    asm volatile("wrmsr" :: "c"(msr_nr), "d"(edx), "a"(eax));
+}
 
 #endif /* _HYVEMIND_X64_PROCESSOR_H */
 
