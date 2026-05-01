16cdde0d5ceabad5ca0bbe023a3042e82bedc541
arm1nt <bmini8664@gmail.com>
2026-05-01 20:09:56 +0200

    Add macros for common bit-level operations

--- hypervisor/src/include/types.h
+++ hypervisor/src/include/types.h
@@ -1,13 +1,35 @@
 #ifndef _HYVEMIND_TYPES_H
 #define _HYVEMIND_TYPES_H
 
+/* TODO: rename to 'bitops.h' or similar */
+
 #include <stdint.h>
+
+#define U8(val)     ((uint8_t)  (val))
+#define U16(val)    ((uint16_t) (val))
+#define U32(val)    ((uint32_t) (val))
+#define U64(val)    ((uint64_t) (val))
+
+#define U64_UPPER32(val)    (U32(val >> 32))
+#define U64_LOWER32(val)    (U32((val << 32) >> 32))
+
+#define U64_X_LSBS_SET(nr) (~(~(U64(0))<<nr))
+
+/* Caller is responsible for correct types */
+#define SET_BIT(val, mask)      (val | mask)
+#define CLEAR_BIT(val, mask)    (val & (~mask))
+#define TOGGLE_BIT(val, bit)    (val ^ mask)
+
+#define IS_SET(val, mask)       (val & mask)
+#define IS_CLEAR(val, mask)     (~(val | mask))
 
 #define BUILD_LEFT_SHIFT(type, val, shift)  (((type) val) << shift)
 #define BUILD_RIGHT_SHIFT(type, val, shift) (((type) val) >> shift)
 
 #define U8_LSHIFT(val, shift) BUILD_LEFT_SHIFT(uint8_t, val, shift)
 #define U8_RSHIFT(val, shift) BUILD_RIGHT_SHIFT(uint8_t, val, shift)
+
+#define U8_WRAP_RSHIFT(val) (U8_LSHIFT(val, 7) | U8_RSHIFT(val, 1))
 
 #define U64_LSHIFT(val, shift) BUILD_LEFT_SHIFT(uint64_t, val, shift)
 #define U64_RSHIFT(val, shift) BUILD_RIGHT_SHIFT(uint64_t, val, shift)
