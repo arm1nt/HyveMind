dd81379eb5b2561c898dbc3d610a92e23b06bf52
arm1nt <bmini8664@gmail.com>
2026-05-01 10:03:14 +0200

    Print call-site information in the 'die' function

--- hypervisor/src/include/fatal.h
+++ hypervisor/src/include/fatal.h
@@ -1,11 +1,17 @@
 #ifndef _HYVEMIND_FATAL_H
 #define _HYVEMIND_FATAL_H
 
+#include "printf.h"
+
 #define __no_return __attribute__((noreturn))
 
+#define die() __die(__FILE__, __LINE__)
+
 static void __no_return
-die(void)
+__die(const char *file, const int line)
 {
+    printf("Termination initiated from '%s:TODO'", file);
+
     for (;;) {
         asm volatile ("hlt");
     }
