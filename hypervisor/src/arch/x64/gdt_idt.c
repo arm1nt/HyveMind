4d5375b34ce8e7268210c5f80bb89258a346d2dc
arm1nt <bmini8664@gmail.com>
2026-05-13 18:36:57 +0200

    Add logic to setup IDT and install IDT handlers

--- hypervisor/src/arch/x64/gdt_idt.c
+++ hypervisor/src/arch/x64/gdt_idt.c
@@ -1,10 +1,68 @@
 #include "gdt_idt.h"
+#include "idt.h"
 #include "string.h"
+#include "printf.h"
 
 extern void reload_cs_register(segment_selector_t cs_selector);
 
 static struct gdt_struct gdt;
 static struct idt_struct idt;
+
+void
+c_div_exception_handler(void)
+{
+    printf("Inside the c_div_exception_handler function");
+}
+
+void
+c_double_fault_handler(void)
+{
+    printf("Inside the c_double_fault_handler function");
+}
+
+static idt_gate_t
+generate_idt_gate(const uint64_t handler_addr)
+{
+    idt_gate_t gate;
+
+    gate.selector = hyvemind_cs_selector;
+    set_idt_gate_offset(&gate, handler_addr);
+    gate.idt_bits = INTERRUPT_IDT_BITS;
+    gate.reserved = U32(0);
+
+    return gate;
+}
+
+static inline void
+register_idt_handler(struct idt_struct *idt, const int index, const idt_gate_t gate)
+{
+    memcpy(&idt->gates[index], &gate, sizeof(idt_gate_t));
+}
+
+#define REGISTER_INTERRUPT_HANDLER(index, handler) \
+    register_idt_handler(&idt, index, generate_idt_gate((uint64_t) handler))
+
+static inline void
+load_idt(const struct idt_struct *idt)
+{
+    const idt_ptr_t install_ptr = {
+        .limit = IDT_LIMIT(IDT_ENTRIES),
+        .base = (uint64_t) idt
+    };
+
+    asm volatile ("LIDT %0" :: "m"(install_ptr));
+}
+
+static void
+setup_idt(void)
+{
+    memset(&idt, 0, sizeof(struct idt_struct));
+
+    REGISTER_INTERRUPT_HANDLER(DIVIDE_ERROR_VECTOR, asm_div_exception_handler);
+    REGISTER_INTERRUPT_HANDLER(DOUBLE_FAULT_VECTOR, asm_double_fault_handler);
+
+    load_idt(&idt);
+}
 
 static void __attribute__((noinline))
 reload_segment_registers(const struct segment_regs *regs)
@@ -41,11 +99,11 @@ setup_gdt(void)
     reload_segment_registers(&regs);
 }
 
-int
+int inline
 setup_gdt_idt(void)
 {
     setup_gdt();
-
+    setup_idt();
     return 0;
 }
 
