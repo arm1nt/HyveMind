1ba2c86bbeb9506200ad53dad7268a3d338b2ca9
arm1nt <bmini8664@gmail.com>
2026-05-29 15:09:35 +0200

    Add custom stdlib header with common helper macros

---
+++ hypervisor/src/include/hyvstdlib.h
@@ -0,0 +1,12 @@
+#ifndef _HYVEMIND_HYVSTDLIB_H
+#define _HYVEMIND_HYVSTDLIB_H
+
+#define MAX(x,y) (((x) < (y)) ? (y) : (x))
+#define MIN(x,y) (((x) < (y)) ? (x) : (y))
+
+#define ROUND_UP(x, y) ((x + ((y) - 1)) / (y))
+
+#define IS_POWER_OF_TWO(x) (((x) & ((x) - 1)) == 0)
+
+#endif /* _HYVEMIND_HYVSTDLIB_H */
+
