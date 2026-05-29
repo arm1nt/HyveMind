90ffe81c9d6e0df5a2bb192ef5138e21d694a923
arm1nt <bmini8664@gmail.com>
2026-05-29 18:44:47 +0200

    Add header with common mm related typedefs

---
+++ hypervisor/src/include/mm_types.h
@@ -0,0 +1,14 @@
+#ifndef _HYVEMIND_MM_TYPES_H
+#define _HYVEMIND_MM_TYPES_H
+
+#include "stdint.h"
+
+/* Marker for verbosity */
+#define __directly_mapped
+
+typedef uint64_t virt_addr_t;
+typedef uint64_t phys_addr_t;
+typedef uint64_t pfn_t;
+
+#endif /* _HYVEMIND_MM_TYPES_H */
+
