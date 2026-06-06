8c4aab8448b5763796e936de1ed8b7db8ec9db0f
arm1nt <bmini8664@gmail.com>
2026-06-06 11:53:03 +0200

    Add function to terminate the hypervisor with an extra msg

--- hypervisor/src/include/fatal.h
+++ hypervisor/src/include/fatal.h
@@ -2,19 +2,28 @@
 #define _HYVEMIND_FATAL_H
 
 #include "printf.h"
+#include "types.h"
 
 #define __no_return __attribute__((noreturn))
 
 #define die() __die(__FILE__, __LINE__)
+#define die_reason(msg) __die_reason(__FILE__, __LINE__, (msg))
 
-static void __no_return
+static inline void __no_return
 __die(const char *file, const int line)
 {
-    printf("Termination initiated from '%s:TODO'", file);
+    printf("Termination initiated from '%s:%lu'", file, U64(line));
 
     for (;;) {
         asm volatile ("hlt");
     }
+}
+
+static inline void __no_return
+__die_reason(const char *file, const int line, const char *msg)
+{
+    printf("%s", msg);
+    __die(file, line);
 }
 
 #endif /* _HYVEMIND_FATAL_H */
