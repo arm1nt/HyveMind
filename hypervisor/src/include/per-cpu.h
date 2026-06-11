c01ed387701bb8936d9f3ec0165d0fd302dcb0f5
arm1nt <bmini8664@gmail.com>
2026-06-11 11:41:59 +0200

    Add convenience per-cpu macros that resolve the current cpuid automatically

--- hypervisor/src/include/per-cpu.h
+++ hypervisor/src/include/per-cpu.h
@@ -19,11 +19,15 @@
 #define DEFINE_PER_CPU_VAL(_type, _name, _val)              \
     DEFINE_PER_CPU(_type, _name) = {[0 ... HYVEMIND_MAX_NR_CPUS-1] = (_val) }
 
+extern int get_current_cpuid(void);
 
-#define percpu_ptr(_name, _cpu) ((void *) &(__percpu_##_name[_cpu]))
-#define percpu_val(_name, _cpu) (__percpu_##_name[_cpu])
+#define percpu_ptr(_name) percpu_ptr_raw(_name, get_current_cpuid())
+#define percpu_ptr_raw(_name, _cpu) ((void *) &(__percpu_##_name[_cpu]))
+#define percpu_val(_name) percpu_val_raw(_name, get_current_cpuid())
+#define percpu_val_raw(_name, _cpu) (__percpu_##_name[_cpu])
 
-#define set_percpu_val(_name, _cpu, _val) (__percpu_##_name[_cpu] = (_val))
+#define set_percpu_val(_name, _val) set_percpu_val_raw(_name, get_current_cpuid(), _val)
+#define set_percpu_val_raw(_name, _cpu, _val) (__percpu_##_name[_cpu] = (_val))
 
 #endif /* _HYVEMIND_PER_CPU_H */
 
