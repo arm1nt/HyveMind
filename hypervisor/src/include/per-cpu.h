896459303d89f0d761197e568c0b4008dcc8116a
arm1nt <bmini8664@gmail.com>
2026-06-11 10:48:27 +0200

    Add a header to allow defining, declaring and accessing per cpu variables

---
+++ hypervisor/src/include/per-cpu.h
@@ -0,0 +1,29 @@
+#ifndef _HYVEMIND_PER_CPU_H
+#define _HYVEMIND_PER_CPU_H
+
+#ifndef HYVEMIND_MAX_NR_CPUS
+    #define HYVEMIND_MAX_NR_CPUS    64
+#endif
+
+#define __percpu
+
+#define DECLARE_PER_CPU(_type, _name)                       \
+    extern typeof(_type) __percpu_##_name[HYVEMIND_MAX_NR_CPUS]
+
+#define DEFINE_PER_CPU(_type, _name)                        \
+    typeof(_type) __percpu_##_name[HYVEMIND_MAX_NR_CPUS]
+
+#define DEFINE_PER_CPU_ALIGNED(_type, _name, _alignment)    \
+    DEFINE_PER_CPU(_type, _name) __attribute__((aligned(_alignment)))
+
+#define DEFINE_PER_CPU_VAL(_type, _name, _val)              \
+    DEFINE_PER_CPU(_type, _name) = {[0 ... HYVEMIND_MAX_NR_CPUS-1] = (_val) }
+
+
+#define percpu_ptr(_name, _cpu) ((void *) &(__percpu_##_name[_cpu]))
+#define percpu_val(_name, _cpu) (__percpu_##_name[_cpu])
+
+#define set_percpu_val(_name, _cpu, _val) (__percpu_##_name[_cpu] = (_val))
+
+#endif /* _HYVEMIND_PER_CPU_H */
+
