88f30dd892afece7883b3922054a914c8659c236
arm1nt <bmini8664@gmail.com>
2026-07-19 16:13:08 +0200

    Declare per-cpu 'max_phys_addr' in the mm.h header

--- hypervisor/src/arch/x64/include/asm/mm.h
+++ hypervisor/src/arch/x64/include/asm/mm.h
@@ -2,6 +2,9 @@
 #define _HYVEMIND_X64_ASM_MM_H
 
 #include <stdint.h>
+#include "per-cpu.h"
+
+DECLARE_PER_CPU(uint64_t, max_phys_addr);
 
 extern uint64_t max_phys_addr;
 #define MAX_PHYS_ADDR max_phys_addr;
