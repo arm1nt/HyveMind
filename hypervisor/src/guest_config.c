5c35ed3ab3d965beced58b3c0399e8f1a75aa56f
arm1nt <bmini8664@gmail.com>
2026-07-30 16:26:58 +0200

    Add function to map memory information in the guest config to bytes

--- hypervisor/src/guest_config.c
+++ hypervisor/src/guest_config.c
@@ -1,4 +1,10 @@
+#include "types.h"
 #include "guest_config.h"
+#include "printf.h"
+
+#define MEM_GRANULARITY_KB_SHIFT 10
+#define MEM_GRANULARITY_MB_SHIFT 20
+#define MEM_GRANULARITY_GB_SHIFT 30
 
 /* Hardcode for now, but change later */
 #define GUEST_CFG_NUMBER_OF_GUESTS 1
@@ -7,8 +13,8 @@ static struct guest_config guest_configs[GUEST_CFG_NUMBER_OF_GUESTS] = {
     {
         .name = "vm1",
         .nr_vcpus = 1,
-        .mem_size = 300,
-        .mem_granularity = MB,
+        .mem_size = 1024,
+        .mem_granularity = GB,
         .bzImageName = "vm1-bzImage",
         .initramfsName = "vm1-initramfs",
     },
@@ -21,5 +27,23 @@ get_guest_configs(void)
     info.nr_guests = GUEST_CFG_NUMBER_OF_GUESTS;
     info.guest_configs = guest_configs;
     return info;
+}
+
+uint64_t
+get_req_mem_size_bytes(const struct guest_config *config)
+{
+    switch (config->mem_granularity) {
+        case BYTES:
+            return config->mem_size;
+        case KB:
+            return U64_LSHIFT(config->mem_size, MEM_GRANULARITY_KB_SHIFT);
+        case MB:
+            return U64_LSHIFT(config->mem_size, MEM_GRANULARITY_MB_SHIFT);
+        case GB:
+            return U64_LSHIFT(config->mem_size, MEM_GRANULARITY_GB_SHIFT);
+        default:
+            pr_error("VM configuration specifies unsupported memory granularity");
+            return 0;
+    }
 }
 
--- hypervisor/src/include/guest_config.h
+++ hypervisor/src/include/guest_config.h
@@ -1,6 +1,8 @@
 #ifndef _HYVEMIND_GUEST_CONFIG_H
 #define _HYVEMIND_GUEST_CONFIG_H
 
+#include "hyvstdlib.h"
+
 enum mem_granularity {
     BYTES,
     KB,
@@ -12,7 +14,7 @@ struct guest_config {
     char *name;
 
     unsigned int nr_vcpus;
-    unsigned int mem_size;
+    uint64_t mem_size;
     enum mem_granularity mem_granularity;
 
     /* Names as specified in the limine config */
@@ -27,6 +29,7 @@ struct guest_config_info {
 };
 
 struct guest_config_info get_guest_configs(void);
+uint64_t get_req_mem_size_bytes(const struct guest_config *config);
 
 #endif /* _HYVEMIND_GUEST_CONFIG_H */
 
