b7645c2a22c32782ef438eebc4229ee24b2affcb
arm1nt <bmini8664@gmail.com>
2026-09-03 13:50:08 +0200

    Update in/out wrapper function naming

--- hypervisor/src/include/io.h
+++ hypervisor/src/include/io.h
@@ -5,28 +5,28 @@
 
 #define BUILD_IO_OUT(size, type)                                    \
     static inline void                                              \
-    io_out##size(uint16_t io_port, type val)                        \
+    io_out##size(const uint16_t io_port, const type val)            \
     {                                                               \
-        asm volatile ("out %0, %1" :: "a"(val), "d"(io_port));      \
+        asm volatile ("out %0, %1" :: "a"(val), "d"(io_port));     \
     }
 
 #define BUILD_IO_IN(size, type)                                     \
     static inline type                                              \
-    io_in##size(uint16_t io_port)                                   \
+    io_in##size(const uint16_t io_port)                             \
     {                                                               \
-        int ret;                                                    \
-        asm volatile ("in %1, %0" : "=a"(ret) : "d"(io_port));      \
+        type ret;                                                   \
+        asm volatile ("in %1, %0" : "=a"(ret) : "d"(io_port));     \
         return ret;                                                 \
     }
 
 BUILD_IO_OUT(b, uint8_t);
 BUILD_IO_OUT(w, uint16_t);
-BUILD_IO_OUT(d, uint32_t);
+BUILD_IO_OUT(l, uint32_t);
 #undef BUILD_IO_OUT
 
 BUILD_IO_IN(b, uint8_t);
 BUILD_IO_IN(w, uint16_t);
-BUILD_IO_IN(d, uint32_t);
+BUILD_IO_IN(l, uint32_t);
 #undef BUILD_IO_IN
 
 #endif /* _HYVEMIND_IO_H */
